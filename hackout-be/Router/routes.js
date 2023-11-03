c0b92eaf46a7afb5669c33208dba2ba0f3a8cf26
Gargee-Bhowmick <gargee.bhowmick09@gmail.com>
2023-11-03 21:32:56 +0530

    Recommendation endpoint added

--- hackout-be/Router/routes.js
+++ hackout-be/Router/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const preg_breast=require('../Components/pregnancy_breast_feeding')
 const authenticate=require('../JWT/authenticate');
 const medicineTracker = require("../Components/medicineTracker");
-const nutrients = require('../Components/NutriTrack/nutrition')
+const nutrients = require('../Components/NutriTrack/nutrition');
+const recommendations = require('../Components/Recommendations/rec_automation')
 router.post('/precautions',preg_breast);
 router.post('/medicineTracker',medicineTracker);
-router.post('/meal',nutrients)
+router.post('/meal',nutrients);
+router.post('./VideoRecommendations',recommendations);
 module.exports=router
