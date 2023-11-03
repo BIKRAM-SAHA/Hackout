a7595e36c4e2e8e25b6f0210d32cb35101982104
Gargee-Bhowmick <gargee.bhowmick09@gmail.com>
2023-11-04 01:05:45 +0530

    fix

--- hackout-be/Components/Contraction/track_contraction.js
+++ hackout-be/Components/Contraction/track_contraction.js
@@ -4,36 +4,9 @@ const { PrismaClient }=require('@prisma/client')
 const prisma = new PrismaClient()
 
 module.exports=async (req,res)=>{
-    let browser;
     try {
-      let query = req.body.searchQuery;
-      let type = req.body.type; // type = exercise, yoga , music , etc etc depending upon what backend sends us
-      if(!query){
-        if(!type){
-            // write the code 
-        }
-        // write the code to take necessary information from database and make a query string
-      }
-      browser = await puppeteer.launch({ headless: true });
-      const page = await browser.newPage();
-  
-      await page.goto('https://www.youtube.com');
-  
-      await page.type('input[name="search_query"]', query);
-      await page.keyboard.press('Enter');
-  
-      await page.waitForSelector('ytd-video-renderer');
-  
-      const videoLinks = await page.$$eval('ytd-video-renderer', (videoRenderers) => {
-        return videoRenderers.map((videoRenderer) => {
-          const link = videoRenderer.querySelector('a#thumbnail')?.href;
-          return link;
-        });
-      });
-  
-      // Close the browser
-      await browser.close();
-      await res.status(200).send({ success: true, message: "Video links are as follows", data: videoLinks });
+
+      await res.status(200).send({ success: true, message: "Contraction data updated", data: contract });
       return;
     } 
     catch (err) {
