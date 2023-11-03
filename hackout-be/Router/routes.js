aba9bbe6fa3856001ae84fe1b8aafbb3ffa27d05
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-11-03 21:36:33 +0530

    Backend updated

--- hackout-be/Router/routes.js
+++ hackout-be/Router/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const preg_breast=require('../Components/pregnancy_breast_feeding')
 const authenticate=require('../JWT/authenticate');
 const medicineTracker = require("../Components/medicineTracker");
-const nutrients = require('../Components/NutriTrack/nutrition')
+const nutrients = require('../Components/NutriTrack/nutrition');
+const statisticalTracker = require("../Components/StatisticalTracker/statisticalTracker");
 router.post('/precautions',preg_breast);
 router.post('/medicineTracker',medicineTracker);
-router.post('/meal',nutrients)
+router.post('/statisticalTracker',statisticalTracker);
+router.post('/meal',nutrients);
 module.exports=router
