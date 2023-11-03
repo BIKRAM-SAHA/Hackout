202702a736a20d05bcd17a3e6ffbe5b7a6fb60a5
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-11-04 01:02:59 +0530

    Statistical Tracker added

--- hackout-be/Components/StatisticalTracker/statisticalTracker.js
+++ hackout-be/Components/StatisticalTracker/statisticalTracker.js
@@ -4,45 +4,64 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async (req, res) => {
-  const drug_name = req.body.drug_name;
-  if (drug_name === undefined || drug_name===null) {
-    res
-      .status(200)
-      .send({ success: false, message: "Parameter is empty", error: "drug_name length is 0" });
-      return;
-    }
+  const patient_id =req.body.patient_id;
+  // const tracker_id =req.body.tracker_id;
+  const maternal_weight=req.body.maternal_weight || null;
+  const blood_pressure=req.body.blood_pressure || null;
+  const fetal_movement=req.body.fetal_movement || null;
+  const fetal_heart_rate=req.body.fetal_heart_rate || null;
+  const waist_circumference=req.body.waist_circumference || null;
+  const amniotic_fluid_index=req.body.amniotic_fluid_index || null;
+  const blood_sugar_level=req.body.blood_sugar_level || null;
+  const thyroid_function=req.body.thyroid_function || null;
+  const haemoglobin_level=req.body.haemoglobin_level || null;
+  
+    try {
+        const patient = await prisma.patient.findFirst({
+            where: {
+                patient_id: patient_id
+            }
+            }
+        );
+  
+        if (!patient) { //if no such patient is found
+            res
+                .status(200)
+                .send({ success: false, message: "No such patient details exists" });
+            return;
+        }
+        const tracker_id=patient.tracker_id;
+    //     if(details.link_permit_to_purchase){
+    //     details.link_permit_to_purchase = await Promise.all(details.link_permit_to_purchase.map(async (link) => {
+    //         try {
+    //           const a = await prisma.purchase_orders.findUnique({
+    //             where: {
+    //               pk_purchase_order_id: link.fk_purchase_order_id,
+    //             },
+    //           });
+    //           return !a.is_deleted ? link : null;
+    //         } catch (error) {
+    //           throw error;
+    //         }
+    //       }))
+    //       details.link_permit_to_purchase = details.link_permit_to_purchase.filter(link => link !== null);
+    // }
+    const tracker_details=await prisma.tracker.update({
+      where:{
+        pk_tracker_id: tracker_id
+      },
 
-  const dosage_name = await drug_name.toLowerCase();
-  const generic_name = await drug_name.toUpperCase();
-  const description =
-    (await drug_name.charAt(0).toUpperCase()) + (await drug_name.slice(1));
-  const url = `https://api.fda.gov/drug/label.json?search=description:${description}+AND+openfda.generic_name:${generic_name}+AND+dosage_and_administration:${dosage_name}&limit=1`;
-  try {
-    const response = await axios.get(url);
-    if (response.length === 0) {
-      res
-        .status(200)
-        .send({
-          success: false,
-          message: "No Data exists with this Drug Name",
-          data: drug_name,
-        });
-        return;
+
+      
+    })
+        res //if found the returns all the details
+            .status(200)
+            .send({ success: true, message: "Permit details are ", data: details });
+    } catch (err) { //if any error accours in the fetching of permit details
+        res
+            .status(200)
+            .send({ success: false, message: "Error in fetching permit details", data: err });
+    } finally {
+        await prisma.$disconnect()
     }
-    res
-      .status(200)
-      .send({
-        success: true,
-        message: "Data successfully fetched for given drug name",
-        data: response.data.results,
-      });
-  } catch (error) {
-    res
-      .status(200)
-      .send({
-        success: false,
-        message: "Error occured in fetching data",
-        error: error,
-      });
-  }
-};
+}
