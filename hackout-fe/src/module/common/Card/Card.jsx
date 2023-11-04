4dfc0be0369968c16925b0349e770fd0c05dc788
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-11-04 21:20:50 +0530

    commit

--- hackout-fe/src/module/common/Card/Card.jsx
+++ hackout-fe/src/module/common/Card/Card.jsx
@@ -1,10 +1,17 @@
 import React, { useContext, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Card.scss";
-
-function Card() {
-
-  return (<div className="card">Hello</div>);
+import Arrow from "./dropDownArrow.svg";
+function Card({ message }) {
+  return (
+    <div className="card">
+      <div className="leftBorder" />
+      <div className="message">Gestational Age</div>
+      <div className="dropDown">
+        <img src={Arrow} />
+      </div>
+    </div>
+  );
 }
 
 export default Card;
