8cb4642168f9302ec5879b18a229e7270038d7d4
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-11-05 02:35:31 +0530

    node updated

--- hackout-be/Router/routes.js
+++ hackout-be/Router/routes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
-//const preg_breast=require('../Components/pregnancy_breast_feeding')
-//const medicineTracker = require("../Components/medicineTracker");
 const nutrients = require('../Components/NutriTrack/nutrition');
 const statisticalTracker = require("../Components/StatisticalTracker/statisticalTracker");
-const recommendations = require('../Components/Recommendations/rec_automation')
-// router.post('/precautions',preg_breast);
-//router.post('/medicineTracker',medicineTracker);
+const recommendations = require('../Components/Recommendations/rec_automation');
+const EmailTest = require("../Components/EmailTest/EmailTest");
+
 router.post('/nutrient',nutrients);
 router.post('/statisticalTracker',statisticalTracker);
 router.post('/videos',recommendations);
-// router.post('/precautions',preg_breast);
+router.post('/email',EmailTest);
 
 module.exports=router
