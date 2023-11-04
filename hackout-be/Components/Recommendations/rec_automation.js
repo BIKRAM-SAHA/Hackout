c87575dc54c90a8795b98c74e1d8d541d3a37c18
Gargee-Bhowmick <gargee.bhowmick09@gmail.com>
2023-11-04 21:09:46 +0530

    YT Recommendation and sign up done

--- hackout-be/Components/Recommendations/rec_automation.js
+++ hackout-be/Components/Recommendations/rec_automation.js
@@ -8,16 +8,22 @@ module.exports=async (req,res)=>{
     try {
       let patient_id = req.body.patient_id;
       let query = req.body.searchQuery;
-      let type = req.body.type; // type = exercise, yoga , music , etc etc depending upon what backend sends us
-      //const patient_id = req.body.patient_id
-      
+      let type = req.body.type; // type = exercise, yoga , music , motivational-video, tips , etc etc depending upon what backend sends us
+      const patient = await prisma.patient.findFirst({
+         where:{
+          pk_patient_id : patient_id,
+         }
+      })
+      console.log(patient)
       if(!query){
         if(!type){
-            // write the code 
+            return res.status(200).send({success:false,message:"Please provide type - exercise,yoga,music,motivatiional video,tips,etc",data:req.body})
         }
         // write the code to take necessary information from database and make a query string
+        query = type + "for pregnant woman age " + patient.age + "," + patient.trimester + "trimester" + patient.weight + "kg weight"
       }
-      browser = await puppeteer.launch({ headless: true });
+      browser = await puppeteer.launch({ headless: true,
+        args: [`--remote-debugging-port=5000`] });
       const page = await browser.newPage();
   
       await page.goto('https://www.youtube.com');
