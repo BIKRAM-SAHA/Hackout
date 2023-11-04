9f2d22ecb19b76d5eb8d739ae608b3b47b86dc99
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-11-04 20:47:37 +0530

    commit

--- hackout-fe/src/module/StatisticalTrackerPage/StatisticalTrackerPage.jsx
+++ hackout-fe/src/module/StatisticalTrackerPage/StatisticalTrackerPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./StatisticalTracker.scss";
 import { useAuth } from "../common/hooks/useAuth";
+import Card from "../common/Card/Card";
 
 function StatisticalTrackerPage() {
   //   const location = useLocation();
@@ -11,7 +12,26 @@ function StatisticalTrackerPage() {
   //     login({ name: "Bikram" });
   //   }, []);
 
-  return (<div className="header">Helooo</div>);
+  return (
+    <div className="outer">
+      <div className="container">
+        <Card />
+        <Card />
+      </div>
+      <div className="container">
+        <Card />
+        <Card />
+      </div>
+      <div className="container">
+        <Card />
+        <Card />
+      </div>
+      <div className="container">
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default StatisticalTrackerPage;
