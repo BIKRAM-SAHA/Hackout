8fb3ce6f044d67a935659c6704a4d75e75ee185b
BIKRAM-SAHA <bikramlearn15@gmail.com>
2023-11-05 11:37:45 +0530

    fix

--- hackout-be/index.js
+++ hackout-be/index.js
@@ -9,9 +9,13 @@ const password='MamaLearnz'
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const createRoutes=require('./Router/createRoutes')
+var bodyParser = require('body-parser')
 
 app.use(cookieParser());
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
+// parse application/json
 app.use(express.json()); //convert all incoming requests to json
 app.use(cors({ credentials: true })); //abiding by cors policy
 
