09071e2f233c6b4605f40348faf106fddf410272
BIKRAM-SAHA <bikramlearn15@gmail.com>
2023-11-05 11:59:18 +0530

    login done

--- hackout-fe/src/module/AuthPages/Login.jsx
+++ hackout-fe/src/module/AuthPages/Login.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BoxContainer, Input, Label, SubmitButton } from "./Login.styles";
+import { useAuth } from "../common/hooks/useAuth";
 
 function Login() {
+  const { login } = useAuth();
+  const emailRef = useRef();
+  const passwordRef = useRef();
   return (
     <BoxContainer>
       <div className="header">Login</div>
-      <Label htmlFor="name">Name</Label>
-      <Input type="text" id="name" name="name" />
 
       <Label htmlFor="email">Email</Label>
-      <Input type="email" id="email" name="email" />
+      <Input type="email" id="email" name="email" ref={emailRef} />
 
       <Label htmlFor="password">Password</Label>
-      <Input type="password" id="password" name="password" />
-      <SubmitButton type="submit">Submit</SubmitButton>
+      <Input type="password" id="password" name="password" ref={passwordRef} />
+      <SubmitButton
+        type="submit"
+        onClick={() =>
+          login({
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+          })
+        }
+      >
+        Submit
+      </SubmitButton>
     </BoxContainer>
   );
 }
--- hackout-fe/src/module/common/hooks/useAuth.jsx
+++ hackout-fe/src/module/common/hooks/useAuth.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalstorage";
+import axios from "axios";
+import qs from "qs";
 
 const AuthContext = createContext();
 
@@ -10,9 +12,33 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
 
   // call this function when you want to authenticate the user
-  const login = async (data) => {
-    setUser(data);
-    navigate("/");
+  const login = async ({ email, password }) => {
+    let data = qs.stringify({
+      email: email,
+      password: password,
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: `${import.meta.env.VITE_BASE_URL}/patientLogin`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setUser(response.data?.data || null);
+        console.log("loggedIn");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   // call this function to sign out logged in user
